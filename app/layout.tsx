138dbefe6cf1697ae8b27c71be14ba0716f54619
Arjun Nahar <ArjunNahar@dunsterhouse.co.uk>
2023-10-26 17:28:15 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Toaster } from 'sonner'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { ThemeProvider } from './(marketing)/_components/providers/theme-provider'
-import { ConvexClientProvider } from './(marketing)/_components/providers/convex-provider'
+import { ThemeProvider } from '../components/providers/theme-provider'
+import { ConvexClientProvider } from '../components/providers/convex-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
