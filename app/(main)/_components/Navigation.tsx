dc89f92e55f66d54bb779d54be203f14af96e728
Arjun Nahar <ArjunNahar@dunsterhouse.co.uk>
2023-10-26 11:07:46 +0100

    Move documents to trash

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -1,15 +1,23 @@
 "use client"
 import { cn } from '@/lib/utils'
-import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from 'lucide-react'
+import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from 'lucide-react'
 import { usePathname } from 'next/navigation'
 import React, { useRef, ElementRef, useState, useEffect} from 'react'
 import { useMediaQuery } from 'usehooks-ts'
 import { useMutation } from "convex/react";
 import UserItem from './UserItem'
 import { api } from '@/convex/_generated/api'
+
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+  } from "@/components/ui/popover";
+
 import Item from './Item'
 import { toast } from 'sonner'
 import DocumentList from './DocumentList'
+import TrashBox from './TrashBox'
 
 const Navigation = () => {
     const pathname = usePathname();
@@ -134,6 +142,19 @@ const Navigation = () => {
         </div>
         <div className='mt-4'>
             <DocumentList/>
+            <Item
+                onClick={handleCreate}
+                icon={Plus}
+                label='Add a page'
+            />
+            <Popover>
+                <PopoverTrigger className='w-full mt-4'>
+                    <Item label="Trash" icon={Trash}/>
+                </PopoverTrigger>
+                <PopoverContent side={isMobile ? "bottom" : "right"} className='p-0 w-72'>
+                    <TrashBox/>
+                </PopoverContent>
+            </Popover>
         </div>
         <div 
         onMouseDown={handleMouseDown}
