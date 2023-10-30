6b41fdb71b5e8dc2f43ca8c8f3157b2e718f4917
Arjun Nahar <ArjunNahar@dunsterhouse.co.uk>
2023-10-30 17:29:56 +0000

    Icon Picker

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+"use client"
 
-const DocumentIdPage = () => {
+import ToolBar from "@/components/ToolBar"
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel"
+import { useQuery } from "convex/react"
+
+
+interface DocumentIdPageProps{
+  params: {
+    documentId: Id<"documents">
+  }
+}
+
+const DocumentIdPage = ({
+  params
+}: DocumentIdPageProps) => {
+
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId
+  })
+
+  if(document === undefined) {
+    return(
+    <div>
+      Loading...
+    </div>
+    )
+  }
+
+  if (document === null){
+    return <div>Not Found</div>
+  }
   return (
-    <div>DocumentIdPage</div>
+    <div className="pb-40">
+      <div className="h-[35vh]"/>
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <ToolBar initialData={document}/>
+      </div>
+    </div>
   )
 }
 
